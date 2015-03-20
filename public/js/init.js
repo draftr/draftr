cac0b0764dc3874f7426b9a7a0ddf9f5f2259f65
MrBoolean <marcandrebinder@gmail.com>
2015-03-20 12:55:49 +0100

    Add stuff

--- public/js/init.js
+++ public/js/init.js
@@ -11,13 +11,16 @@
       'angular/ui-router': '/deps/angular-ui-router/release/angular-ui-router',
       'bluebird':          '/deps/bluebird/js/browser/bluebird',
       'store':             '/deps/store.js/store+json2.min',
-      'event-emitter':     '/deps/eventemitter2/lib/eventemitter2'
+      'event-emitter':     '/deps/eventemitter2/lib/eventemitter2',
+      'marked':            '/deps/marked/lib/marked',
+      'angular/sanitize':  '/deps/angular-sanitize/angular-sanitize',
     },
     shim: {
       'angular':           { 'exports': 'angular', 'deps': ['jquery'] },
-      'jquery':            { 'exports': 'jQuery' },
-      'angular/ui-router': { 'deps': ['angular'] },
-      'angular/route':     { 'deps': ['angular'] }
+      'jquery':            { 'exports': 'jQuery'   },
+      'angular/ui-router': { 'deps': ['angular']   },
+      'angular/route':     { 'deps': ['angular']   },
+      'angular/sanitize':  { 'deps': ['angular']   }
     },
     config: {
       'draftr/app': [
@@ -27,8 +30,14 @@
         { path: 'draftr/controller/sign-up',           name: 'SignUpController' },
         { path: 'draftr/controller/work-space/list',   name: 'WorkSpaceListController' },
         { path: 'draftr/controller/work-space/detail', name: 'WorkSpaceDetailController' },
+        { path: 'draftr/controller/work-space/write',  name: 'WorkSpaceWriteController' },
         { path: 'draftr/controller/me/profile',        name: 'MeProfileController' },
         { path: 'draftr/controller/me/work-space',     name: 'MeWorkSpaceController' },
+
+        { path: 'draftr/controller/work-space/detail/article', name: 'WorkSpaceDetailArticleController' },
+        { path: 'draftr/controller/work-space/detail/feed',    name: 'WorkSpaceDetailFeedController' },
+        { path: 'draftr/controller/work-space/detail/team',    name: 'WorkSpaceDetailTeamController' },
+        { path: 'draftr/controller/work-space/detail/options', name: 'WorkSpaceDetailOptionsController' },
       ]
     }
   });
