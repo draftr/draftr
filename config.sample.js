d6e20af69344442fdbf9b3c78a7529c06b96b6fe
MrBoolean <marcandrebinder@gmail.com>
2015-04-01 14:20:41 +0200

    Add queue configuration

--- config.sample.js
+++ config.sample.js
@@ -3,8 +3,13 @@
 module.exports = {
   prod: {
     port: 1337,
-    queue: {
-      port: 1338
+    kue: {
+      prefix: 'queue',
+      redis: {
+        port: 6379,
+        host: '127.0.0.1'
+        // auth: 'yourpassword'
+      }
     },
     db: {
       sets: [
